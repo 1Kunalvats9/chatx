2fb89a79fcf87e423a96f857432ffadfa8a3d8ec
Kunal Vats <1kunalvats9@gmail.com>
2025-07-03 01:48:15 +0530

    nativewind setup code done

---
+++ mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-5xl px-4 py-3 w-full">Chatx App beginingg Yayyy!!</Text>
+    </View>
+  );
+}
